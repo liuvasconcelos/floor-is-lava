cdec3668ef14eb83011825d961f01cace6ce88a6
liuvasconcelos <livia.vasconcelos@stant.com.br>
2020-01-02 18:02:40 -0300

    develop Add initial structure
    - Add plane detection delegate
    
    Signed-off-by: liuvasconcelos <livia.vasconcelos@stant.com.br>

--- FloorIsLava/ViewController.swift
+++ FloorIsLava/ViewController.swift
@@ -7,14 +7,37 @@
 //
 
 import UIKit
+import ARKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, ARSCNViewDelegate {
 
+    @IBOutlet weak var sceneView: ARSCNView!
+    
+    let configuration = ARWorldTrackingConfiguration()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.sceneView.delegate = self
+        self.configuration.planeDetection = .horizontal
+        
+        self.sceneView.debugOptions = [SCNDebugOptions.showWorldOrigin, SCNDebugOptions.showFeaturePoints]
+        self.sceneView.session.run(configuration)
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
+        print("---------- new ARPlane Anchor")
     }
 
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
+        print("***** UPDATING ****")
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didRemove node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
+        print("(((( REMOVED ))))")
+    }
 
 }
 
