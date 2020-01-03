34c6bbdb8294eba06439234d0ba63ba8afac8304
liuvasconcelos <livia.vasconcelos@stant.com.br>
2020-01-03 10:11:44 -0300

    develop Add box to scene and make it with gravity
    -
    
    Signed-off-by: liuvasconcelos <livia.vasconcelos@stant.com.br>

--- Vehicle/ViewController.swift
+++ Vehicle/ViewController.swift
@@ -24,23 +24,26 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         self.sceneView.session.run(configuration)
     }
     
-    func createLava(planeAnchor: ARPlaneAnchor) -> SCNNode {
-        let lavaNode = SCNNode(geometry: SCNPlane(width: CGFloat(planeAnchor.extent.x),
-                                                  height: CGFloat(planeAnchor.extent.z)))
-        lavaNode.geometry?.firstMaterial?.diffuse.contents = UIImage(named: "lava")
-        lavaNode.geometry?.firstMaterial?.isDoubleSided = true
-        lavaNode.position = SCNVector3(planeAnchor.center.x,
+    func createConcrete(planeAnchor: ARPlaneAnchor) -> SCNNode {
+        let concreteNode = SCNNode(geometry: SCNPlane(width: CGFloat(planeAnchor.extent.x),
+                                                      height: CGFloat(planeAnchor.extent.z)))
+        concreteNode.geometry?.firstMaterial?.diffuse.contents = UIImage(named: "concrete")
+        concreteNode.geometry?.firstMaterial?.isDoubleSided = true
+        concreteNode.position = SCNVector3(planeAnchor.center.x,
                                        planeAnchor.center.y,
                                        planeAnchor.center.z)
-        lavaNode.eulerAngles = SCNVector3(90.degreesToRadians, 0, 0)
+        concreteNode.eulerAngles = SCNVector3(90.degreesToRadians, 0, 0)
         
-        return lavaNode
+        let staticBody = SCNPhysicsBody.static()
+        concreteNode.physicsBody = staticBody 
+        
+        return concreteNode
     }
     
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
         guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
-        let lavaNode = createLava(planeAnchor: planeAnchor)
-        node.addChildNode(lavaNode)
+        let concreteNode = createConcrete(planeAnchor: planeAnchor)
+        node.addChildNode(concreteNode)
         print("---------- new ARPlane Anchor")
     }
 
@@ -50,7 +53,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             childNode.removeFromParentNode()
         }
         
-        let lavaNode = createLava(planeAnchor: planeAnchor)
+        let lavaNode = createConcrete(planeAnchor: planeAnchor)
         node.addChildNode(lavaNode)
         print("***** UPDATING ****")
     }
@@ -63,10 +66,32 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         print("(((( REMOVED ))))")
     }
-
+    
+    @IBAction func addCar(_ sender: Any) {
+        guard let pointOfView = sceneView.pointOfView else { return }
+        let transform               = pointOfView.transform
+        let orientation             = SCNVector3(-transform.m31, -transform.m32, -transform.m33)
+        let location                = SCNVector3(transform.m41, transform.m42, transform.m43)
+        let currentPositionOfCamera = orientation + location
+        
+        let box = SCNNode(geometry: SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0))
+        box.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
+        box.position = currentPositionOfCamera
+        
+        let body = SCNPhysicsBody(type: .dynamic, shape: SCNPhysicsShape(node: box,
+                                                                         options: [SCNPhysicsShape.Option.keepAsCompound: true]))
+        box.physicsBody = body
+        
+        sceneView.scene.rootNode.addChildNode(box)
+    }
+    
 }
 
 extension Int {
     var degreesToRadians: Double { return Double(self) * .pi/180}
+}
+
+func +(left: SCNVector3, right: SCNVector3) -> SCNVector3 {
+    return SCNVector3Make(left.x + right.x, left.y + right.y, left.z + right.z)
 }
 
